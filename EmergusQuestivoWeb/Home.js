3fc9238343ac4ce4a657b45b7a8c1058a1f7bd8f
Anthony Smith <smith2619@saskpolytech.ca>
2020-05-30 20:27:23 -0600

    Did stuff

--- EmergusQuestivoWeb/Home.js
+++ EmergusQuestivoWeb/Home.js
@@ -14,20 +14,20 @@
         }
     }
 
-    let startTiles = [
-        ['f', 'f', 'f', 'c', 'f', 'f', 'k'],
-        ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
-        ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
-        ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
-        ['f', 'c', 'p', 'c', 'f', 'c', 'f'],
-        ['w', 'f', 'f', 'f', 'f', 'f', 'w'],
-        ['w', 'w', 'w', 'w', 'w', 'w', 'w']
-    ];
+    //let startTiles = [
+    //    ['f', 'f', 'f', 'c', 'f', 'f', 'k'],
+    //    ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
+    //    ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
+    //    ['f', 'c', 'f', 'c', 'f', 'c', 'f'],
+    //    ['f', 'c', 'p', 'c', 'f', 'c', 'f'],
+    //    ['w', 'f', 'f', 'f', 'f', 'f', 'w'],
+    //    ['w', 'w', 'w', 'w', 'w', 'w', 'w']
+    //];
 
-    var startRoom = new room(startTiles, [true, true, false, true], "Start Room");
+    //var startRoom = new room(startTiles, [true, true, false, true], "Start Room");
 
     //Tracking the player icon's position
-    var playerPos = [0, 0];
+    var playerPos = [3, 4];
     var roomList = [];
 
     // The initialize function must be run each time a new page is loaded.
@@ -71,9 +71,11 @@
                 move('r');
             });
 
-            setCellSizes();
+            //setCellSizes();
             roomList = makeRooms();
             //loadSampleData();
+            moveRoom(roomList[0]);
+            //Set player location
             
             // Add a click event handler for the highlight button.
             $('#highlight-button').click(roomRender);
@@ -81,45 +83,99 @@
         });
     };
 
+    //Changing the current room.
+    function moveRoom(newRoom) {
+        Excel.run(function (ctx) {
+            var currentSheet = ctx.workbook.worksheets.getActiveWorksheet();
+
+            var sheets = ctx.workbook.worksheets;
+            var newSheet = sheets.add(newRoom.title);
+            sheets.load("items/name");
+            newSheet.activate();
+            currentSheet.delete();
+            setCellSizes();
+            roomRender(newRoom);
+
+            return ctx.sync();
+            //return context.sync()
+            //    .then(function () {
+            //        if (sheets.items.length === 1) {
+            //            console.log("Can't delete the last sheet");
+            //        } else {
+            //            currentSheet.delete();
+            //        }
+            //    });
+        });
+    }
+
+    // Resizes the cells of the play area so that they are (more or less) square.
     function setCellSizes() {
         Excel.run(function (ctx) {
             var sheet = ctx.workbook.worksheets.getActiveWorksheet();
             var cellRange = sheet.getRanges("a1:k11");
 
             var internalRange = sheet.getRanges("c3:i9");
-            cellRange.format.fill.color = "black";
+            cellRange.format.fill.color = "White";
             internalRange.format.fill.color = "yellow";
             cellRange.format.columnWidth = 20;
             cellRange.format.rowHeight = 20;
             cellRange.format.horizontalAlignment = "Center";
             cellRange.format.verticalAlignment = "Center";
             cellRange.format.font.size = 15;
-            cellRange.format.font.color = "white";
+            cellRange.format.font.color = "Black";
             return ctx.sync();
         }).catch(errorHandler);
     }
 
     //Render rooms with a 2 cell pad on top and left sides (top left room edge starts at Row 3, Column C)
     function roomRender(newRoom) {
-        newRoom = startRoom;
+        //newRoom = roomList[0];
 
         Excel.run(function (ctx) {
             var sheet = ctx.workbook.worksheets.getActiveWorksheet();
-            var cellRange = sheet.getRange("c3:i9");
+            //var wallRange = sheet.getRange("b2:j10");
+            //wallRange.load("value, rowCount, columnCount");
+            //c3:i9
+
+            var cellRange = sheet.getRange("b2:j10");
             cellRange.load("value, rowCount, columnCount");
 
             return ctx.sync().then(function () {
+                //wallRange.format.fill.color = "SaddleBrown"; //#8B4513
+
+                //if (newRoom.doors[0] >= 0) {
+                //    wallRange.getCell(0, 4).format.fill.color = "Orange"; //#FFA500
+                //}
+                //if (newRoom.doors[1] >= 0) {
+                //    wallRange.getCell(4, 8).format.fill.color = "Orange";
+                //}
+                //if (newRoom.doors[2] >= 0) {
+                //    wallRange.getCell(8, 4).format.fill.color = "Orange";
+                //}
+                //if (newRoom.doors[3] >= 0) {
+                //    wallRange.getCell(4, 0).format.fill.color = "Orange";
+                //}
+
                 for (var i = 0; i < cellRange.rowCount; i++) {
                     for (var j = 0; j < cellRange.columnCount; j++) {
                         switch (newRoom.tiles[i][j]) {
-                            case 'f':
-                                cellRange.getCell(i, j).format.fill.color = "brown";
+                            case 0:
+                                cellRange.getCell(i, j).format.fill.color = "Black";
                                 break;
-                            case 'w':
-                                cellRange.getCell(i, j).format.fill.color = "blue";
+                            case 1:
+                                cellRange.getCell(i, j).format.fill.color = "LightGrey";
                                 break;
-                            case 'c':
-                                cellRange.getCell(i, j).format.fill.color = "black";
+                            case 2:
+                                cellRange.getCell(i, j).fromat.fill.color = "AntiqueWhite";
+                                break;
+                            case 3:
+                                cellRange.getCell(i, j).format.fill.color = "DodgerBlue";
+                                break;
+                            case 4:
+                                cellRange.getCell(i, j).format.fill.color = "SaddleBrown"; //#8B4513
+                                break;
+                            case 5:
+                                cellRange.getCell(i, j).format.fill.color = "Yellow"; //#FFFF00
                                 break;
                             case 'k':
                                 cellRange.getCell(i, j).format.fill.color = "brown";
@@ -139,6 +195,9 @@
                                 break;
                         }
                     }
+
+                    //Display the player
+                    cellRange.getCell(playerPos[0], playerPos[1]).values = '☺';
                 }
             }).then(ctx.sync);
                 
@@ -149,28 +208,65 @@
     //and if it's possible to move to the cell, 
     //move the player icon
     //and update player position
-    function move(direction) {
+    function move(direction, currentRoom) {
         //showNotification("Dir: " + direction +  " Position: " + playerPos[0] + ", " + playerPos[1]);
         //Switch for directions
         switch (direction) {
             case 'u':
                 if (playerPos[0] > 0) {
-                    checkValidMove(playerPos[0] - 1, playerPos[1]);
+                    //checkValidMove(playerPos[0] - 1, playerPos[1]);
+                    switch (checkValidMove(playerPos[0] - 1, playerPos[1])) {
+                        case 0:
+                            movePlayerIcon(playerPos[0] - 1, playerPos[1]);
+                            break;
+                        case 1:
+                            moveRoom(roomList[currentRoom.doors[0]]);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 break;
             case 'd':
-                if (playerPos[0] < 6) {
-                    checkValidMove(playerPos[0] + 1, playerPos[1]);
+                if (playerPos[0] < 8) {
+                    switch (checkValidMove(playerPos[0] + 1, playerPos[1])) {
+                        case 0:
+                            movePlayerIcon(playerPos[0] + 1, playerPos[1]);
+                            break;
+                        case 1:
+                            moveRoom(roomList[currentRoom.doors[2]]);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 break;
             case 'l':
                 if (playerPos[1] > 0) {
-                    checkValidMove(playerPos[0], playerPos[1] - 1);
+                    switch (checkValidMove(playerPos[0], playerPos[1] - 1)) {
+                        case 0:
+                            movePlayerIcon(playerPos[0], playerPos[1] - 1);
+                            break;
+                        case 1:
+                            moveRoom(roomList[currentRoom.doors[3]]);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 break;
             case 'r':
-                if (playerPos[1] < 6) {
-                    checkValidMove(playerPos[0], playerPos[1] + 1);
+                if (playerPos[1] < 8) {
+                    switch (checkValidMove(playerPos[0], playerPos[1] + 1)) {
+                        case 0:
+                            movePlayerIcon(playerPos[0], playerPos[1] + 1);
+                            break;
+                        case 1:
+                            moveRoom(roomList[currentRoom.doors[1]]);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 break;
             default:
@@ -180,9 +276,12 @@
 
     //Helper function for checking if a given cell is valid for movement
     function checkValidMove(row, col) {
+        var rVal = -1;
+
         Excel.run(function (ctx) {
+            //var rVal = -1;
             var sheet = ctx.workbook.worksheets.getActiveWorksheet();
-            var cellRange = sheet.getRange("c3:i9");
+            var cellRange = sheet.getRange("b2:j10");
             //cellRange.load("format/fill/color");
             var props = cellRange.getCellProperties({
                 format: {
@@ -194,18 +293,25 @@
             return ctx.sync().then(function () {
                 //console.log(props.value);
                 console.log(props.value[row][col].format.fill.color == "#A52A2A");
-                if (props.value[row][col].format.fill.color == "#A52A2A") {
-                    movePlayerIcon(row, col);
+                if (props.value[row][col].format.fill.color == "#D3D3D3") {
+                    //movePlayerIcon(row, col);
+                    rVal = 0;
+                } else if (props.value[row][col].format.fill.color == "#FFFF00") {
+                    rVal = 1;
                 }
+
+                return rVal;
             }).then(ctx.sync);
         }).catch(errorHandler);
+
+        return rVal;
     }
 
     //Helper function for moving the player icon and updating the player position
     function movePlayerIcon(newRow, newCol) {
         Excel.run(function (ctx) {
             var sheet = ctx.workbook.worksheets.getActiveWorksheet();
-            var cellRange = sheet.getRange("c3:i9");
+            var cellRange = sheet.getRange("b2:j10");
 
             return ctx.sync().then(function () {
                 cellRange.getCell(playerPos[0], playerPos[1]).values = '';
@@ -216,6 +322,7 @@
         }).catch(errorHandler);
     }
 
+    // Save this for the "Ron's Coming!" button
     function loadSampleData() {
         var values = [
             [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)],
@@ -278,21 +385,24 @@
         roomList = [];
 
         /*
-         *  0 = Chasm Tile
-         *  1 = Floor Tile
+         *  0 = Chasm tile
+         *  1 = Floor tile
          *  2 = Sand tile
          *  3 = Water tile
          *  4 = Wall tile
+         *  5 = Door tile
          */
 
         let startTiles = [
-            [1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0]
+            [4, 4, 4, 5, 5, 5, 4, 4, 4],
+            [4, 1, 1, 1, 1, 1, 1, 1, 4],
+            [4, 1, 1, 1, 1, 1, 1, 1, 4],
+            [4, 1, 1, 1, 1, 1, 1, 1, 4],
+            [4, 1, 1, 1, 1, 1, 1, 1, 4],
+            [4, 1, 1, 1, 1, 1, 1, 1, 4],
+            [4, 0, 0, 0, 1, 0, 0, 0, 4],
+            [4, 0, 0, 0, 1, 0, 0, 0, 4],
+            [4, 4, 4, 4, 5, 4, 4, 4, 4]
         ];
         let startRoomItems = [
             {
@@ -591,7 +701,7 @@
         let roundItems = [];
         let roundTitle = "E Pluribus Anus"; //Temp Name
         let roundDesc = "";
-        let roundRoom = new room(roundTiles, [-1, -1, -1, 9], roundTitle, roundDesc, roundItems);
+        let roundRoom = new room(roundRoomTiles, [-1, -1, -1, 9], roundTitle, roundDesc, roundItems);
         roomList.push(roundRoom);
 
         let commaRoomTiles = [
@@ -672,7 +782,7 @@
         ];
         let torchIslandTitle = "Torch Island";
         let torchIslandDesc = "";
-        let torchIslandRoom = new room(bunnyEarTiles, [-1, 14, 15, 12], torchIslandTitle, torchIslandDesc, torchIslandItems);
+        let torchIslandRoom = new room(torchIslandTiles, [-1, 14, 15, 12], torchIslandTitle, torchIslandDesc, torchIslandItems);
         roomList.push(torchIslandRoom);
 
         let snorkelTiles = [
